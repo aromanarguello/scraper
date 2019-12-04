070f6c8dbf498bdeb8d6c024c96cabc3d6a09228
Alejandro <aromanarguello@gmail.com>
2019-12-03 22:41:16 -0500

    setup scrapy

--- prepi/spiders/allrecipes_spider.py
+++ prepi/spiders/allrecipes_spider.py
@@ -1,5 +1,20 @@
 import scrapy
 
 
-class AllRecipesSPider(scrapy.Spider):
-    name = "AllRecipes"
+class AllRecipesSpider(scrapy.Spider):
+    name = "all_recipes"
+    start_urls = [
+        'https://www.myrecipes.com/recipe/turkey-and-swiss-pinwheels']
+
+    def start_request(self):
+        urls = [
+            'https://www.myrecipes.com/recipe/turkey-and-swiss-pinwheels'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        filename = "test.html"
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
